1199415f2bb5b926395f381fa81a907ed36951e5
Andrey <andrey23arce@gmail.com>
2024-06-10 15:40:44 -0700

    Create Help.js
    Add new component Help

---
+++ src/Components/js/Help.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./Navbar";
+import BackgroundImage from "./Background";
+
+function Help (){
+    return (
+        <div>
+            <NavBar />
+            <BackgroundImage src="https://i.ibb.co/D7pVW8y/bg-pethub-2.jpg" />
+            <h1 className="text-center mt-4 mb-4">AYUDA</h1>
+        </div>
+    )
+}
